5bedb8cb5c0cc522143a2aab7014a71f259b08b2
yuki0035vn@gmail.com <yuki0035vn@gmail.com>
2019-07-07 16:10:41 +0700

    CreateRoom

---
+++ CL/api_gamebai/api_gamebai/Models/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api_gamebai.Models
+{
+    public class Notification
+    {
+        public string response;
+        public string message;
+
+        public Notification()
+        {
+        }
+
+        public Notification(string res,string mess)
+        {
+            this.response = res;
+            this.message = mess;
+        }
+    }
+}
