a29bd0fac651a4ed98bff6ce46c183e7517b75c4
đỗ kim đăng khoa <43430177+kakoi-3nhan4-16@users.noreply.github.com>
2019-07-14 15:38:01 +0700

    update

---
+++ gameBai/Assets/Script/Huong Dan/SaveData_V01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveData_V01 : MonoBehaviour
+{
+   public InputField input;
+   public void Save()
+    {
+        if (input)
+        {
+            PlayerPrefs.SetString("name",input.text);           
+        }
+    }
+    public void Load()
+    {
+        if (input)
+        {
+            input.text = PlayerPrefs.GetString("namesadasd");
+        }
+    }
+}
