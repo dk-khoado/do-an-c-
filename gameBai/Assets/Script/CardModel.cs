48c0acc931e0f1c3d305a10682c4e9220e02f3e3
yuki0035vn@gmail.com <yuki0035vn@gmail.com>
2019-07-16 14:31:01 +0700

    Resouces
    Image

---
+++ gameBai/Assets/Script/CardModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu(fileName = "Card Game",menuName = "Card/Card Uno")]
+public class CardModel : ScriptableObject
+{
+    public int number;
+    public Color_Card color;
+    public bool isChucNang;
+    public Sprite image;
+}
+public enum Color_Card
+{
+    Red,Blue,Green,Yellow, Black
+}
