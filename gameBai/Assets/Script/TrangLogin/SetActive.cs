4fc6a27099f8093c28e6a0598a5013cc77ef757c
Kirin9586 <43573859+Kirin9586@users.noreply.github.com>
2019-07-17 21:41:48 +0700

    login
    Login

---
+++ gameBai/Assets/Script/TrangLogin/SetActive.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActive : MonoBehaviour
+{
+    public GameObject Login;
+    public GameObject Register;
+    public void ChangeLogin()
+    {
+        Login.SetActive(true);
+        Register.SetActive(false);
+    }
+    public void ChangeRegister()
+    {
+        Login.SetActive(false);
+        Register.SetActive(true);
+    }
+}
