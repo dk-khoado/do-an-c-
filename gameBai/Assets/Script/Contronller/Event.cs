6b00841db94d5ede7778fd98ec8919542534348f
đỗ kim đăng khoa <43430177+kakoi-3nhan4-16@users.noreply.github.com>
2019-07-21 16:47:21 +0700

    update game bai

---
+++ gameBai/Assets/Script/Contronller/Event.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Event : EventTrigger
+{
+    public override void OnBeginDrag(PointerEventData data)
+    {
+        Debug.Log("OnBeginDrag called.");
+    }
+}
---
+++ gameBai/Assets/Script/UI/UI_manager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_manager : MonoBehaviour
+{
+    public CardModel properties;
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private Image colorCurrent;
+    public GameObject UIselectColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (properties)
+        {
+            image.enabled = true;
+        }
+        else
+        {
+            image.enabled = false;
+        }
+        if (properties)
+        {
+            image.sprite = properties.image;
+            switch (properties.color)
+            {
+                case Color_Card.Red:
+                    colorCurrent.color = Color.red;
+                    break;
+                case Color_Card.Blue:
+                    colorCurrent.color = Color.blue;
+                    break;
+                case Color_Card.Green:
+                    colorCurrent.color = Color.green;
+                    break;
+                case Color_Card.Yellow:
+                    colorCurrent.color = Color.yellow;
+                    break;                
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void ShowSelectColor()
+    {
+        UIselectColor.SetActive(true);
+    }
+    public void CloseSelectColor()
+    {
+        UIselectColor.SetActive(false);
+    }
+}
